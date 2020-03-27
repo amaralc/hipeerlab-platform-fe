b3aa5c5dfa8d885295206b2336cd6f3caecf56ec
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-03-27 17:14:07 -0300

    FE-CLIENT / Recoloca linhas na tabela
    não está sendo possível colocar border-radius

--- src/components/Layouts/Accountability/styles.js
+++ src/components/Layouts/Accountability/styles.js
@@ -6,13 +6,12 @@ import styled from "styled-components";
 export const Table = styled.table`
   width: 100%;
   margin-bottom: 1rem;
-  border: 1px solid rgba(0, 0, 0, 0.125);
   border-radius: 5px !important;
 
   th,
   td {
     padding: 0.75rem;
-    /*border: 1px solid rgba(0, 0, 0, 0.125);*/
+    border: 1px solid rgba(0, 0, 0, 0.125);
     text-align: left;
   }
   th {
