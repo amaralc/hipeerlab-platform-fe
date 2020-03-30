c4a132ffe96cb49229d28c5b2262a0222ecfdbd7
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-03-30 17:44:49 -0300

    FE-CLIENT / Aplica texto justificado

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,7 +2,8 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
   p {
-    text-indent:40px
+    text-indent:40px;
+    text-align: justify;
   }
   body {
     -webkit-font-smoothing: antialiased;
