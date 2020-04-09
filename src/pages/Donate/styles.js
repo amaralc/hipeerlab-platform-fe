217d8b9e7fa6e2c3bd0ed0879ec886822b749dbe
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-04-09 13:01:41 -0300

    FEATURE / STYLE / Estiliza botão de prestacao de contas

---
+++ src/pages/Donate/styles.js
@@ -0,0 +1,11 @@
+/* --------------------------------- IMPORTS ---------------------------------*/
+import styled from "styled-components";
+
+/* --------------------------------- EXPORTS ---------------------------------*/
+export const Btn = styled.a`
+  background-color: #343a40 !important;
+  &:hover,
+  &:focus {
+    background: #f4d75d !important;
+  }
+`;
