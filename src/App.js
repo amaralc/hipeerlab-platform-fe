98fb91044a5524abffa2d8fe6fe69dec40daadd1
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-04-11 13:21:55 -0300

    FEATURE / CONFIG / Configura store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Provider } from "react-redux";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import GlobalStyle from "./styles/global";
@@ -7,16 +8,20 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./config/ReactotronConfig";
 
+import { store } from "./store";
+
 export default class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Routes />
-        <NavBar />
-        <GlobalStyle />
-        <main>{this.props.children}</main>
-        <Footer />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes />
+          <NavBar />
+          <GlobalStyle />
+          <main>{this.props.children}</main>
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,9 +1,8 @@
-const INITIAL_STATE = {
-};
+const INITIAL_STATE = {};
 
 export default function auth(state = INITIAL_STATE, action) {
-    switch (action.type) {
-      default:
-        return: state;
-    }
+  switch (action.type) {
+    default:
+      return state;
+  }
 }
