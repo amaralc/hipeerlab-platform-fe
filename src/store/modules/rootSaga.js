e315cd0f60bc208afa4af8eea703dfe34f292829
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-04-11 13:09:16 -0300

    FEATURE / CONFIG / Configura modulos

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -1,11 +1,10 @@
 /**
  * unir todos os sagas
  */
-import { all } from 'redux-saga/effects';
+import { all } from "redux-saga/effects";
 
-import auth from './auth/sagas';
-import user from './user/sagas';
+import auth from "./auth/sagas";
 
 export default function* rootSaga() {
-  return yield all([auth, user]);
+  return yield all([auth]);
 }
