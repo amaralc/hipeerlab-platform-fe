e64312cf9151a47e4bab94daa91cdb0f5aefb634
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-04-11 10:50:33 -0300

    FIX / Corrige fragmentação no arquivo de equipamentos

--- src/pages/Equipment/index.js
+++ src/pages/Equipment/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import api from "../../services/api";
 
 import Card from "../../components/Card";
-import Header from "../../components/Header"
+import Header from "../../components/Header";
 
 import FaceShield from "../../img/faceShield.png";
 
@@ -19,25 +19,26 @@ export default class Equipment extends Component {
 
     return (
       <>
-      <Header headerTitle="Equipamentos" />
-      <div className="container">
-        <div className="row">
-          {equipment.map((equipment) => (
-            <div className="col-md-3 mb-5">
-              <Card
-                key={equipment.serial_number}
-                title={equipment.equipment_name}
-                location={equipment.company}
-                updatedAt={equipment.model}
-                status={equipment.state}
-                imgPath={FaceShield}
-                url="https://www.instagram.com/hipeerlab/"
-                button="Ver Equipamento"
-              />
-            </div>
-          ))}
+        <Header headerTitle="Equipamentos" />
+        <div className="container">
+          <div className="row">
+            {equipment.map((equipment) => (
+              <div className="col-md-3 mb-5">
+                <Card
+                  key={equipment.serial_number}
+                  title={equipment.equipment_name}
+                  location={equipment.company}
+                  updatedAt={equipment.model}
+                  status={equipment.state}
+                  imgPath={FaceShield}
+                  url="https://www.instagram.com/hipeerlab/"
+                  button="Ver Equipamento"
+                />
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
