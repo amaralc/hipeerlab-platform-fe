a19d6d44545d3efebcfc68936c3a908392d2ed22
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-04-13 16:40:31 -0300

    FIX / Importa history no app,js e muda o Router utilizado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from "react";
+import { Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import GlobalStyle from "./styles/global";
-import Routes from "./routes";
-import { BrowserRouter } from "react-router-dom";
 
 import "./config/ReactotronConfig";
 
+import Routes from "./routes";
+
+import history from "./services/history";
 import { store } from "./store";
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter>
+        <Router history={history}>
           <Routes />
           <NavBar />
           <GlobalStyle />
           <main>{this.props.children}</main>
           <Footer />
-        </BrowserRouter>
+        </Router>
       </Provider>
     );
   }
