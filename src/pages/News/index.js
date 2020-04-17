828b3c37dff535f17b8629044a5d29c0c55057a7
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2020-04-17 20:17:04 -0300

    FEATURE / CONTENT / Upload two news

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Header from "../../components/Header";
 import NewsCard from "../../components/NewsCard";
-import sample_thumb from "./img/sample.png";
+import ufsc_masks from "./img/ufsc_masks.png";
+import casos_sc from "./img/casos_sc.png";
 
 export default class News extends React.Component {
   render() {
@@ -14,52 +15,20 @@ export default class News extends React.Component {
               <div>
                 <div className="py-1 mb-1">
                   <NewsCard
-                    title="Noticia 1"
-                    date="DD/MM/2020"
-                    description="Texto de exemplo"
-                    source="Google"
-                    link="https://www.google.com"
-                    image={sample_thumb}
+                    title="UFSC ensina a fazer mascaras faciais"
+                    date="16/04/2020"
+                    description="[...] especialistas apontam que as máscaras caseiras podem proteger contra infecções respiratórias."
+                    source="UFSC"
+                    link="https://noticias.ufsc.br/2020/04/faca-a-sua-uso-obrigatorio-de-mascaras-faciais-a-partir-desta-sexta/"
+                    image={ufsc_masks}
                   />
-                </div>
-                <div className="py-1 mb-1">
                   <NewsCard
-                    title="Noticia 2"
-                    date="DD/MM/2020"
-                    description="Texto de exemplo"
-                    source="Google"
-                    link="https://www.google.com"
-                    image={sample_thumb}
-                  />
-                </div>
-                <div className="py-1 mb-1">
-                  <NewsCard
-                    title="Noticia 3"
-                    date="DD/MM/2020"
-                    description="Texto de exemplo"
-                    source="Google"
-                    link="https://www.google.com"
-                    image={sample_thumb}
-                  />
-                </div>
-                <div className="py-1 mb-1">
-                  <NewsCard
-                    title="Noticia 4"
-                    date="DD/MM/2020"
-                    description="Texto de exemplo"
-                    source="Google"
-                    link="https://www.google.com"
-                    image={sample_thumb}
-                  />
-                </div>
-                <div className="py-1 mb-1">
-                  <NewsCard
-                    title="Noticia 5"
-                    date="DD/MM/2020"
-                    description="Texto de exemplo"
-                    source="Google"
-                    link="https://www.google.com"
-                    image={sample_thumb}
+                    title="Uso de máscara se torna obrigatório em SC; número de mortes por coronavírus chega a 31"
+                    date="17/04/2020"
+                    description="Estado tem 962 casos."
+                    source="G1"
+                    link="https://g1.globo.com/sc/santa-catarina/noticia/2020/04/17/numero-de-mortes-por-coronavirus-em-santa-catarina-chega-a-31.ghtml"
+                    image={casos_sc}
                   />
                 </div>
               </div>
