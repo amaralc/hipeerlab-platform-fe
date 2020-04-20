e4c0411ecede9062e12b2942177b90ee536610d3
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-04-20 17:20:15 -0300

    FEATURE / STANDARDIZE / Padroniza Componentes

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../img/icon.png";
+import logo from "../../img/icon.png";
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
