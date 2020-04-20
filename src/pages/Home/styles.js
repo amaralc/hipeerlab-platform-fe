0085b47b3c2dee4d1c911faaa680a7211e951e49
amaralc <amaral.calil@gmail.com>
2020-04-20 13:13:47 -0300

    FEATURE / STYLE / Atualiza estilo da pagina home

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,21 @@
+/* --------------------------------- IMPORTS ---------------------------------*/
+import styled from "styled-components";
+
+/* --------------------------------- EXPORTS ---------------------------------*/
+
+export const Div = styled.div`
+  margin: auto;
+`;
+export const Row = styled.div`
+  margin-bottom: 80px;
+  margin-top: 80px;
+`;
+export const Title = styled.div`
+  -ms-flex: 0 0 100%;
+  flex: 0 0 100%;
+  max-width: 100%;
+  p {
+    text-align: left;
+    font-size: 30px;
+  }
+`;
