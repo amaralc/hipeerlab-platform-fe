519852370ea4c9f0ad51a13ecd86f3ab8971d68a
amaralc <amaral.calil@gmail.com>
2020-04-20 13:13:33 -0300

    FEATURE / CONTENT / Atualiza conteúdo da página Index

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,7 +9,9 @@ import Pronto from "../../img/pronto.png";
 import Lmp from "../../img/lmp.png";
 import Comunave from "../../img/comunave.png";
 
-import { TeamCard, TeamImg, Div, Row, Title } from "../Partner/styles";
+import { Div, Title } from "./styles";
+
+import { TeamCard, TeamImg, Row } from "../Partner/styles";
 
 import Header from "../../components/Header";
 
@@ -23,6 +25,47 @@ export default class Home extends React.Component {
           headerText="Indexação dos projetos de equipamentos hospitalares emergenciais para auxílio no combate à COVID-19."
         />
         <div className="container">
+          <div className="row">
+            <div className="col-md-8 mb-5">
+              <Title>
+                <p>
+                  O que fazemos
+                </p>
+              </Title>
+              <hr />
+              <p>
+                Formamos um grande laboratório, distribuído nas casas dos membros
+                e em espaços físicos de outras instituições parceiras. Cada um
+                de nós contribui com sua iniciativa, equipamentos e habilidades,
+                da maneira como pode e quando tem disponibilidade.
+              </p>
+              <p>
+                
+              </p>
+              
+            </div>
+            <div className="col-md-4 mb-5">
+              <Title><p>O que fazemos</p></Title>
+              <hr />
+              <address>
+                <strong>LMP</strong>
+                <br />
+                R. Eng. Agronômico Andrei Cristian Ferreira, s/n
+                <br />
+                Trindade, Florianópolis, SC
+                <br />
+                CEP 88040-900
+                <br />
+              </address>
+              <address>
+                <abbr title="Phone">P:</abbr>
+                +55 (48) 3271-7229
+                <br />
+                <abbr title="Email">E:</abbr>
+                <a href="mailto:#"> lmp@ufsc.br</a>
+              </address>
+            </div>
+          </div>
           <div className="row">
             <div className="col-md-4 py-3 mb-3">
               <Card
@@ -57,7 +100,8 @@ export default class Home extends React.Component {
                 button="Ver Projetos"
               />
             </div>
-          </div>
+          </div>       
+
           <Row className="row">
             <Title className="py-3 mb-3">
               <p>Parceiros</p>
