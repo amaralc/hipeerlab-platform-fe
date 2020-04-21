407d6bc826dcd05051fe53396945c67229865887
amaralc <amaral.calil@gmail.com>
2020-04-21 18:00:21 -0300

    FIX  / STYLE / Altera estilio de botão da página de contribuição

--- src/pages/Donate/styles.js
+++ src/pages/Donate/styles.js
@@ -14,39 +14,33 @@ export const Title = styled.div`
 `;
 
 export const Button = styled.button`
-  position: relative;
-  z-index: 2;
   display: inline-block;
-  margin: 0 0 0;
-  background-color: #343a40;
-  color: #fff;
   font-weight: 400;
-  border: 1px solid transparent;
-  border-radius: 0.3rem;
-  font-size: 1.25rem;
-  line-height: 1.5;
   text-align: center;
   vertical-align: middle;
   -webkit-user-select: none;
   -moz-user-select: none;
   -ms-user-select: none;
-  line-height: 1.5;
-  padding: 0.5rem 1rem;
-  vertical-align: middle;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
   user-select: none;
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: 0.25rem;
   transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
     border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  color: #fff;
+  background-color: #000;
+  border-color: #000;
 
   &:hover {
-    background: #f4d75d;
+    background: #007bff;
+    border-color: #007bff;
     text-decoration: none;
   }
   &:focus {
     outline: 0;
-    box-shadow: 0 0 0 0.2rem #f4d75d;
+    box-shadow: 0 0 0 0.2rem #007bff;
     z-index: 3;
   }
   @media (prefers-reduced-motion: reduce) {
