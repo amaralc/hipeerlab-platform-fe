046d7ad3482baf9ced701b3e925e2142eb7ea45d
amaralc <amaral.calil@gmail.com>
2020-04-24 03:12:59 -0300

    FIX / CONFIG / Identa codigo e adiciona espacamentos

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,40 +1,43 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+
   footer,
   header {
     display: block;
   }
+
   body {
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-    "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
-    "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-  font-size: 1rem;
-  font-weight: 400;
-  line-height: 1.5;
-  color: #212529;
-  text-align: left;
-  background-color: #fff;
-}
-hr {
-  box-sizing: content-box;
-  height: 0;
-  overflow: visible;
-}
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
+      "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #212529;
+    text-align: left;
+    background-color: #fff;
+  }
 
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  margin-top: 0;
-  margin-bottom: 0.5rem;
-}
+  hr {
+    box-sizing: content-box;
+    height: 0;
+    overflow: visible;
+  }
 
-p {
-  margin-top: 0;
-  margin-bottom: 1rem;
-}
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    margin-top: 0;
+    margin-bottom: 1rem;
+  }
 `;
