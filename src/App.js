45387b6a3f5cd9602200e4b9e6a246c7ae15428d
amaralc <amaral.calil@gmail.com>
2020-04-23 22:36:14 -0300

    FEATURE / Importa reactotron em App.js e reestrutura arquivo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+/* --------------------------------- IMPORTS ---------------------------------*/
 import React from 'react';
 import { Router } from 'react-router-dom';
+import './config/ReactotronConfig';
 
 import Routes from './routes';
 import history from './services/history';
 
+/* --------------------------------- CONTENT ---------------------------------*/
+/* --------------------------------- EXPORTS ---------------------------------*/
 export default function App() {
   return (
     <Router history={history}>
