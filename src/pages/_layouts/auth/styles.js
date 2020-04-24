9304d75bea8f6b40285785e295b5f8fd6f6db0b9
amaralc <amaral.calil@gmail.com>
2020-04-24 03:11:37 -0300

    FIX / STYLES / Corrige layout das paginas default e auth

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -1,28 +1,14 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: 100%;
   width: 100%;
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-right: auto;
-  margin-left: auto;
-  min-width: 992px !important;
-  background-color: #fff;
+  background-color: #000;
 
-  @media (min-width: 576px) {
-    max-width: 540px;
+  h1 {
+    color: #fff;
   }
 
-  @media (min-width: 768px) {
-    max-width: 720px;
-  }
-
-  @media (min-width: 992px) {
-    max-width: 960px;
-  }
-
-  @media (min-width: 1200px) {
-    max-width: 1140px;
+  p {
+    color: rgba(255, 255, 255, 0.5) !important;
   }
 `;
