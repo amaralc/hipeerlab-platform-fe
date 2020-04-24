6f86b779477fedd5bd2c63561c610284c090d2a2
amaralc <amaral.calil@gmail.com>
2020-04-23 22:09:45 -0300

    FEATURE / Adiciona browser history

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
 import Routes from './routes';
+import history from './services/history';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
 }
