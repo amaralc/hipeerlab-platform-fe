36d0b69fb911821260e42b6518c6fe8f9ac0da3c
amaralc <amaral.calil@gmail.com>
2020-04-25 17:39:45 -0300

    FEATURE / CONTENT / Adiciona componentes Input e Form e function handleSubmit

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 import Header from '../../components/Header';
 import { Wrapper, Content } from './styles';
 
-// import { Container } from './styles';
-
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <Header headerTitle="Sign In" />
       <Wrapper>
         <Content>
           <h5>Faça o login</h5>
-          <form>
-            <input type="email" placeholder="Digite seu e-mail" />
-            <input type="password" placeholder="Digite sua senha" />
+          <Form onSubmit={handleSubmit}>
+            <Input name="email" type="email" placeholder="Digite seu e-mail" />
+            <Input
+              name="password"
+              type="password"
+              placeholder="Digite sua senha"
+            />
             <button type="submit">Acessar</button>
             <Link to="/signup">Criar conta</Link>
-          </form>
+          </Form>
         </Content>
       </Wrapper>
     </>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 import Header from '../../components/Header';
 import { Wrapper, Content } from './styles';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <Header headerTitle="Sign Up" />
       <div className="container">
         <Wrapper>
           <Content>
-            <form>
+            <Form onSubmit={handleSubmit}>
               <h5>Crie sua conta</h5>
-              <input placeholder="Nome completo" />
-              <input type="email" placeholder="Digite seu e-mail" />
-              <input type="password" placeholder="Digite sua senha" />
+              <Input name="name" placeholder="Nome completo" />
+              <Input
+                name="email"
+                type="email"
+                placeholder="Digite seu e-mail"
+              />
+              <Input
+                name="password"
+                type="password"
+                placeholder="Digite sua senha"
+              />
               <button type="submit">Acessar</button>
               <Link to="/signin">Já sou cadastrado</Link>
-            </form>
+            </Form>
           </Content>
         </Wrapper>
       </div>
