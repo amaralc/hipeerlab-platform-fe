e2a567809cf0acb8a4e6b31d6d22b0f42095410f
amaralc <amaral.calil@gmail.com>
2020-04-25 20:41:52 -0300

    CONFIG / Configura reactotron para se comunicar com redux

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,12 +1,18 @@
 /* --------------------------------- IMPORTS ---------------------------------*/
 import Reactotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 /* --------------------------------- CONTENT ---------------------------------*/
 /**
  * Se usuário estiver em ambiente de desenvolvimento, conecta reactotron
  */
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure().connect();
+  const tron = Reactotron.configure()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect();
+
   tron.clear();
 
   /**
