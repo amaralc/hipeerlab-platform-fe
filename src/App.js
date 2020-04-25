85abc8c73d99ec168613521be1076b2d7ee9b4fa
amaralc <amaral.calil@gmail.com>
2020-04-25 20:41:05 -0300

    FEATURE / Adiciona componente Provider e importa store a App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 /* --------------------------------- IMPORTS ---------------------------------*/
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 import history from './services/history';
+
+/**
+ * Importação do store precisa vir depois da importação do reactotron
+ */
+import store from './store';
 import GlobalStyle from './styles/global';
 
-/* --------------------------------- CONTENT ---------------------------------*/
 /* --------------------------------- EXPORTS ---------------------------------*/
 export default function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
