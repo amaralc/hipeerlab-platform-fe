f5dabe43e6bf85092550a2aa3981d045518df2b7
amaralc <amaral.calil@gmail.com>
2020-04-25 15:49:03 -0300

    FIX / STYLE / Adiciona margem ao tag <h5>

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -3,5 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   h5 {
     color: #000;
+    margin-bottom: 1.5rem;
   }
 `;
--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -3,5 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   h5 {
     color: #38b6ff;
+    margin-bottom: 1.5rem;
   }
 `;
