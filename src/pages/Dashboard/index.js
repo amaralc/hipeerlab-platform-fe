9f76487c96a9c4798608fb6eded2d904c2d0349b
amaralc <amaral.calil@gmail.com>
2020-04-26 18:05:24 -0300

    FEATURE / LOGIC / Cria teste de chamada tipo GET para api

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from '../../components/Header';
+import api from '~/services/api';
 
 // import { Container } from './styles';
 
 export default function Dashboard() {
+  api.get('appointments');
   return (
     <>
       <Header headerTitle="Dashboard" />
