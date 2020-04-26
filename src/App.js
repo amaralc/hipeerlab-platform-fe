19e437a2f9ced635c410691de71b7d85c5d754e4
amaralc <amaral.calil@gmail.com>
2020-04-26 12:32:58 -0300

    FEATURE / CONFIG / Importa e usa componente PersistGate em App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /* --------------------------------- IMPORTS ---------------------------------*/
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
@@ -10,17 +11,19 @@ import history from './services/history';
 /**
  * Importação do store precisa vir depois da importação do reactotron
  */
-import store from './store';
+import { store, persistor } from './store';
 import GlobalStyle from './styles/global';
 
 /* --------------------------------- EXPORTS ---------------------------------*/
 export default function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
