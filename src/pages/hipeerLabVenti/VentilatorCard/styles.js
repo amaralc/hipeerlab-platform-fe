1251ba10b2111a6bc1bf69efc123f9b0237fe1ce
Artur Della Favera <arturdellafavera@gmail.com>
2020-04-27 15:56:15 -0300

    FEATURE / STYLE / Estiliza card da página hipeerLabVenti

---
+++ src/pages/hipeerLabVenti/VentilatorCard/styles.js
@@ -0,0 +1,46 @@
+/* --------------------------------- IMPORTS ---------------------------------*/
+import styled from 'styled-components';
+
+/* --------------------------------- EXPORTS ---------------------------------*/
+
+export const CardContainer = styled.div`
+  position: relative;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-direction: column;
+  flex-direction: row;
+  min-width: 0;
+  word-wrap: break-word;
+  background-color: #fff;
+  background-clip: border-box;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  border-radius: 0.25rem;
+  height: 100% !important;
+
+  img {
+    border-top-left-radius: calc(0.25rem - 1px);
+    border-top-right-radius: calc(0.25rem - 1px);
+    border: 1.3px solid rgba(0, 0, 0, 0.125);
+  }
+`;
+export const Body = styled.div`
+  -ms-flex: 1 1 auto;
+  flex: 1 1 auto;
+  padding: 1.25rem;
+
+
+  h5 {
+    margin-bottom: 0.75rem;
+  }
+
+  p:last-child {
+    margin-bottom: 0;
+  }
+
+  h5,
+  p {
+    text-align: left !important;
+    font-size: 20px
+  }
+`;
+
