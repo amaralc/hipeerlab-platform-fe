c4c1b9bfa6db33c3360dab930c956d0ecd9bc113
amaralc <amaral.calil@gmail.com>
2020-05-01 20:35:34 -0300

    FEATURE / STYLE / Adiciona bordas e estiliza texto e divs

--- src/pages/hipeerLabVenti/VentilatorCard/styles.js
+++ src/pages/hipeerLabVenti/VentilatorCard/styles.js
@@ -3,6 +3,15 @@ import styled from 'styled-components';
 
 /* --------------------------------- EXPORTS ---------------------------------*/
 
+export const Row = styled.div`
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+`;
+
 export const CardContainer = styled.div`
   position: relative;
   display: -ms-flexbox;
@@ -13,35 +22,35 @@ export const CardContainer = styled.div`
   word-wrap: break-word;
   background-color: #fff;
   background-clip: border-box;
-  border: 1px solid rgba(0, 0, 0, 0.125);
   height: 100% !important;
   border-radius: 6px;
-  padding: 8px 8px;
+  padding: 0.5rem;
   width: 100%;
 
   img {
-    border-top-left-radius: calc(0.25rem - 1px);
-    border-top-right-radius: calc(0.25rem - 1px);
+    border-radius: 0.5rem;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+    width: 100%;
   }
 `;
 export const Body = styled.div`
   -ms-flex: 1 1 auto;
   flex: 1 1 auto;
-  padding: 1.25rem;
+  padding: 0.5rem;
 
-
-  h5 {
-    margin-bottom: 0.75rem;
+  h3 {
+    font-size: 1.25rem;
   }
 
-  p:last-child {
-    margin-bottom: 0;
-  }
-
-  h5,
-  p {
+  h4 {
+    color: rgba(0, 0, 0, 0.3);
+    margin-bottom: 0.5rem;
+    font-size: 0.75rem;
     text-align: left !important;
-    font-size: 20px
+  }
+
+  p {
+    text-align: justify !important;
+    font-size: 1rem;
   }
 `;
-
