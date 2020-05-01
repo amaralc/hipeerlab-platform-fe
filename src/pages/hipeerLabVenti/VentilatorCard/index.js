44bf8faa9bdc06527f06112db0223da17e8917c7
amaralc <amaral.calil@gmail.com>
2020-05-01 20:35:11 -0300

    FEATURE / CONTENT / Adiciona classes e remove texto dos cards

--- src/pages/hipeerLabVenti/VentilatorCard/index.js
+++ src/pages/hipeerLabVenti/VentilatorCard/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { CardContainer, Body } from './styles';
+import { CardContainer, Body, Row } from './styles';
 
 export default function Card(props) {
   return (
-    <CardContainer>
-      <img src={props.imgPath} alt="" />
-      <Body>
-        <h5>{props.title}</h5>
-        <p>{props.status}</p>
-      </Body>
-    </CardContainer>
+    <div className="container">
+      <Row>
+        <div className="col-md-4 mb-1">
+          <CardContainer>
+            <img src={props.imgPath} alt="" />
+          </CardContainer>
+        </div>
+        <div className="col-md-8 mb-1">
+          <Body>
+            <h4>{props.date}</h4>
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+          </Body>
+        </div>
+      </Row>
+    </div>
   );
 }
