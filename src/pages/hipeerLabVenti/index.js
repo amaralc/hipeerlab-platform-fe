af98ede390525241023c777b2485495bcdceb718
amaralc <amaral.calil@gmail.com>
2020-05-02 00:13:10 -0300

    FEATURE / CONTENT / Altera update 2 e adiciona updates 3 e 4

--- src/pages/hipeerLabVenti/index.js
+++ src/pages/hipeerLabVenti/index.js
@@ -5,6 +5,8 @@ import VentilatorCard from './VentilatorCard';
 import Update0 from './img/update-0.png';
 import Update1 from './img/update-1.png';
 import Update2 from './img/update-2.png';
+import Update3 from './img/update-3.png';
+import Update4 from './img/update-4.png';
 
 export default function hipeerLabVenti() {
   return (
@@ -31,12 +33,33 @@ export default function hipeerLabVenti() {
             </Title>
           </div>
           <VentilatorCard
+            date="14/04/2020"
+            title="Corte das chapas de acrílico no NGD"
+            description="As chapas de acrílico do protótipo experimental
+            OxyGEN M V5, adaptado pela equipe hipeerLab, são fabricadas por corte
+            a laser no Núcleo de Gestão e Design - Laboratório de Design e Usabilidade
+            da UFSC."
+            imgPath={Update4}
+          />
+          <VentilatorCard
+            date="13/04/2020"
+            title="Adaptação do projeto Oxygen para transmissão direta"
+            description="Eliminação da transmissão por engrenagens e adaptação de
+            flange formada por anéis de acrílico. A opção reduz a complexidade
+            de montagem do protótipo e se assemelha ao conceito IP."
+            imgPath={Update3}
+          />
+          <VentilatorCard
             date="07/04/2020"
             title="Adoção do projeto OxyGEN"
-            description="Segundo projeto. Baseado no conceito do respirador OxyGEN."
+            description="Após avaliar e comparar outros projetos de grupos do
+            Brasil e do mundo, optamos por adotar o projeto OxyGEN, da equipe
+            Protofy, de Barcelona na Espanha. O projeto é um dos mais bem
+            documentados dentre os projetos abertos (open-source) e faz uso
+            de poucos componentes eletrônicos, assim como de sistema de acionamento
+            com componentes altamente disponíveis no mercado brasileiro."
             imgPath={Update2}
           />
-
           <VentilatorCard
             date="28/03/2020"
             title="Modelagem de conceito com came e cinto"
