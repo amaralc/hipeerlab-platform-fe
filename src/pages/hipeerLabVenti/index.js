ac5d44a24f43081320b708322655d9423bc1e906
amaralc <amaral.calil@gmail.com>
2020-05-01 22:33:13 -0300

    FEATURE / CONTENT / Adiciona update 0 e troca nome de imagens

--- src/pages/hipeerLabVenti/index.js
+++ src/pages/hipeerLabVenti/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Header from '../../components/Header';
 import { Title } from './styles';
 import VentilatorCard from './VentilatorCard';
-import Ventilator2 from './img/update-2.png';
-import Ventilator1 from './img/update-1.png';
+import Update0 from './img/update-0.png';
+import Update1 from './img/update-1.png';
+import Update2 from './img/update-2.png';
 
 export default function hipeerLabVenti() {
   return (
@@ -29,24 +30,28 @@ export default function hipeerLabVenti() {
               <hr />
             </Title>
           </div>
-          <div className="col-md-12 py-9 mb-3">
-            <VentilatorCard
-              date="07/04/2020"
-              title="Adoção do projeto OxyGEN"
-              description="Segundo projeto. Baseado no conceito do respirador OxyGEN."
-              imgPath={Ventilator2}
-            />
-          </div>
-          <div className="col-md-12 py-3 mb-3">
-            <VentilatorCard
-              date="28/03/2020"
-              title="Modelagem de conceito com came e cinto"
-              description="Desenvolvimento de modelo de automatizador de ambu baseado
+          <VentilatorCard
+            date="07/04/2020"
+            title="Adoção do projeto OxyGEN"
+            description="Segundo projeto. Baseado no conceito do respirador OxyGEN."
+            imgPath={Update2}
+          />
+
+          <VentilatorCard
+            date="28/03/2020"
+            title="Modelagem de conceito com came e cinto"
+            description="Desenvolvimento de modelo de automatizador de ambu baseado
               em rotação contínua de um came que traciona cinto atravez de sistema de
               rolos móveis."
-              imgPath={Ventilator1}
-            />
-          </div>
+            imgPath={Update1}
+          />
+          <VentilatorCard
+            date="23/03/2020"
+            title="Rascunho do primeiro conceito"
+            description="Desenvolvimento de modelo utilizando dreno de sucção como
+            alternativa em situação de escassez de ambu bag."
+            imgPath={Update0}
+          />
         </div>
       </div>
     </div>
