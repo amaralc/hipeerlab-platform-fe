d80a938513d2e47670d64b83cbdf47533b6771a6
amaralc <amaral.calil@gmail.com>
2020-05-03 13:48:51 -0300

    FEATURE / CONTENT / Adiciona AvatarInput na pagina de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,6 +4,7 @@ import { Form, Input } from '@rocketseat/unform';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
+import AvatarInput from './AvatarInput';
 import Header from '../../components/Header';
 import UserHeader from '~/components/UserHeader';
 
@@ -24,6 +25,7 @@ export default function Profile() {
         <UserHeader />
         <Container>
           <Form initialData={profile} onSubmit={handleSubmit}>
+            <AvatarInput name="avatar_id" />
             <Input name="name" placeholder="Nome completo" />
             <Input name="email" type="email" placeholder="Seu e-mail" />
 
