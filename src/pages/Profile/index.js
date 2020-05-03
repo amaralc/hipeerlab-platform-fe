6ac8e77c0fb3ba61af89498dac23315962691f3a
amaralc <amaral.calil@gmail.com>
2020-05-03 13:08:06 -0300

    FEATURE / LOGIC / Importa useDispatch do redux e substitui console.tron.log por updateProfileRequest

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
-import { useSelector } from 'react-redux';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import Header from '../../components/Header';
 import UserHeader from '~/components/UserHeader';
@@ -8,10 +10,11 @@ import UserHeader from '~/components/UserHeader';
 import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector((state) => state.user.profile);
 
   function handleSubmit(data) {
-    console.tron.log(data);
+    dispatch(updateProfileRequest(data));
   }
 
   return (
