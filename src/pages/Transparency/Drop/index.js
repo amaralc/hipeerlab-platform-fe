9adefc412c6f5e9679b7b4d3647c84c0ef65d98a
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-05-06 14:45:15 -0300

    FEATURE / LOGIC / Cria estado global

--- src/pages/Transparency/Drop/index.js
+++ src/pages/Transparency/Drop/index.js
@@ -1,39 +1,41 @@
 import React, { Component } from 'react';
-
-// import { Container } from './styles';
+import { Title } from '../styles';
 
 export default class Drop extends Component {
+  state = {
+    selected: '1',
+  };
+
+  handleSubmit = async (e) => {
+    const x = document.getElementById('mySelect').value;
+
+    if (x === 'Financas') {
+      this.setState({
+        selected: '1',
+      });
+    }
+
+    if (x === 'Producao') {
+      this.setState({
+        selected: '2',
+      });
+    }
+  };
+
   render() {
-    return <form id="app-cover">
-    <div id="select-box">
-            <input type="checkbox" id="options-view-button">
-            <div id="select-button" class="brd">
-                    <div id="selected-value">
-                        <span>Select a platform</span>
-                    </div>
-                    <div id="chevrons">
-                            <i class="fas fa-chevron-up"></i>
-                            <i class="fas fa-chevron-down"></i>
-                    </div>
-            </div>
-            <div id="options">
-                    <div class="option">
-                            <input class="s-c top" type="radio" name="platform" value="codepen">
-                            <input class="s-c bottom" type="radio" name="platform" value="codepen">
-                            <i class="fab fa-codepen"></i>
-                            <span class="label">Produção</span>
-                            <span class="opt-val">Produção</span>
-                    </div>
-                    <div class="option">
-                            <input class="s-c top" type="radio" name="platform" value="dribbble">
-                            <input class="s-c bottom" type="radio" name="platform" value="dribbble">
-                            <i class="fab fa-dribbble"></i>
-                            <span class="label">Finanças</span>
-                            <span class="opt-val">Finanças</span>
-                    </div>
-                    <div id="option-bg"></div>
-            </div>
-    </div>
-</form>;
+    return (
+      <select id="mySelect" onChange={this.handleSubmit}>
+        <option value="Financas">
+          <Title>
+            <h2>Finanças</h2>
+          </Title>
+        </option>
+        <option value="Produção">
+          <Title>
+            <h2>Produção</h2>
+          </Title>
+        </option>
+      </select>
+    );
   }
 }
