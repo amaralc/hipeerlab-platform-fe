a07fc77fa8c335fac2237148c169fc246dbcc92e
Artur Della Favera <arturdellafavera@gmail.com>
2020-05-08 10:29:51 -0300

    FEATURE / CONTENT / Inicia configuração de faq de respirador

---
+++ src/pages/Faq/Drop/Respirador/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SubTitle, Row } from '../styles';
+
+// import { Container } from './styles';
+
+export default function Respirador() {
+  return (
+    <>
+      <SubTitle>
+        <p>Perguntas sobre o respirador mecânico hipeerLab</p>
+        <hr />
+      </SubTitle>
+    </>
+  );
+}
